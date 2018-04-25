c5153cc5622431a581e28c644dc7e38b4beca09c
mac <collamail@gmail.com>
2018-04-25 08:33:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/onclicklistenertraining/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.onclicklistenertraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        // handling onClick Events
+    }
+}
